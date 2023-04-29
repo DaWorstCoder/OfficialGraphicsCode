4d3e7355d6c889b976d96e951e5bd0f6de129fd4
DaWorstCoder <64881270+DaWorstCoder@users.noreply.github.com>
2023-04-29 02:37:09 +0000

    This is basic code for creating graphical interfaces

--- Main.java
+++ Main.java
@@ -10,29 +10,29 @@ class Main extends JPanel implements ActionListener {
 
 	Main() {
 		JFrame Frame = new JFrame(""); // Insert Name here
-        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Frame.setResizable(false);
-        Frame.pack();
-        Frame.setVisible(true);
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame.setResizable(false);
+		Frame.pack();
+		Frame.setVisible(true);
 		Frame.setSize(width, height);
 		Frame.add(this);
-		
-		this.setPreferredSize(new Dimension(width - 1 , height - 1 + 100));
-        this.setBackground(new Color(33, 33, 33)); //Use this to change the background color
-        this.setFocusable(true);
 
-		/**This can be commented out if a static image is to be produced.**/
+		this.setPreferredSize(new Dimension(width - 1, height - 1 + 100));
+		this.setBackground(new Color(33, 33, 33)); // Use this to change the background color
+		this.setFocusable(true);
+
+		/** This can be commented out if a static image is to be produced. **/
 		timer = new Timer(1, this);
 		timer.start();
 	}
 
-	/**Anything that you want to draw goes in here**/
+	/** Anything that you want to draw goes in here **/
 	public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-		
-    }
+		super.paintComponent(g);
 
-	/**Anything that you want to happened repeatedly goes in here**/
+	}
+
+	/** Anything that you want to happened repeatedly goes in here **/
 	public void actionPerformed(ActionEvent e) {
 		repaint();
 	}
